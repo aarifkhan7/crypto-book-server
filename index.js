89de4010340972aecdb166f26287b4c8698971c4
Aarif Khan <ajajchauhan@gmail.com>
2023-06-22 13:34:15 +0530

    Update index.js add mongo connect

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var app = express();
 
 // port settings
 const port = process.env.PORT || 5000
+const uri = "mongodb+srv://aarifkhan_7:admin@cluster0.zwgc9a4.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+let database = null;
+let records = null;
 
 app.get('/', (req, res)=>{
     res.send('Foo Bar');
@@ -10,6 +23,15 @@ app.get('/', (req, res)=>{
 
 app.listen(port, async () => {
     console.log("Server running on port " + port);
+    try{
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged deployment. Successfully connected to MongoDB!");
+        database = client.db("my-app");
+        records = database.collection("records");;
+    } catch(err) {
+        console.log("Could not connect to MongoDB!");
+    }
 });
 
 module.exports = app;
