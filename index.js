3935abb905e6285098a888574d02dfe96b19dc54
Aarif Khan <ajajchauhan@gmail.com>
2023-06-24 14:16:59 +0530

    config session cookies

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(session({
         maxAge: 1000*60*60*24*30,
         secure: true,
         sameSite: 'none',
-        domain: 'onrender.com'
+        domain: 'crypto-book-frontend.onrender.com'
     },
     store: MongoStore.create({
         client,
